ce92dd24962f17d54343968bc635ccc712e64288
Cristian L. Vlasceanu <cristian@zerobugs.org>
2012-01-29 04:12:44 +0000

    fixed mime-type for dwarfz library sources
    git-svn-id: https://zerobugs.svn.codeplex.com/svn@67494 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

---
+++ tools/svnfix.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Fix svn:mime-type property
+
+dir=${1:-.}
+
+find $dir -name "*.cpp" -exec svn propset svn:mime-type text/plain {} \;
+find $dir -name "*.h" -exec svn propset svn:mime-type text/plain {} \;
+
